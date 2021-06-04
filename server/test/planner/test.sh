27c3017b8723b0c87ee0ee15a3ce9db73b5fdaee
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2021-06-04 14:22:42 +0200

    Testing scripts

--- server/test/planner/test.sh
+++ server/test/planner/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#FILES=("base_1_day.pddl" "base_m1_day.pddl" "big.pddl" "restrictive.pddl")
-FILES=("big.pddl" "restrictive.pddl")
+FILES=("base_1_day.pddl" "base_m1_day.pddl" "big.pddl" "restrictive.pddl")
 METRIC_CONF=("5" "4" "3")
 CBP_HEURISTIC=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 CBP_ALGORITHM=("2" "3" "4" "6" "7" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20")
@@ -10,19 +9,20 @@ CBP_HDIFF=("0" "1" "2")
 mkdir results/
 mkdir results/metric-ff
 mkdir results/cbp
+mkdir results/lpg
 
 #
 # Metric-FF
 #
-#for problem in "${FILES[@]}"
-#do
-#	for conf in "${METRIC_CONF[@]}"
-#	do
-#		echo "Running ${problem}_conf_${conf}.txt"
-#		timeout 5m ./metric-ff -o domain.pddl -f $problem -s $conf > "results/metric-ff/${problem}_conf_${conf}.txt" &
-#	done
-#	wait
-#done
+for problem in "${FILES[@]}"
+do
+	for conf in "${METRIC_CONF[@]}"
+	do
+		echo "Running ${problem}_conf_${conf}.txt"
+		timeout 5m ./metric-ff -o domain.pddl -f $problem -s $conf > "results/metric-ff/${problem}_conf_${conf}.txt" &
+	done
+	wait
+done
 
 #
 # CBP
@@ -47,4 +47,13 @@ do
 			wait
 		done
 	done
+done
+ 
+#
+# LPG
+#
+for problem in "${FILES[@]}"
+do
+	echo "Running ${problem}"
+	timeout 5m ./lpg -o domain.pddl -f $problem -n 100 -cputime 300 -out "results/lpg/${problem}" > "results/metric-ff/${problem}_out.txt"
 done
--- server/test/recommender/test.js
+++ server/test/recommender/test.js
@@ -9,16 +9,16 @@ const Recommender = require('../../recommender/index.js');
 db.sequelize.options.logging = false;
 
 function readCsv(path) {
-	return new Promise((resolve, reject) => {
-		const results = [];
+    return new Promise((resolve, reject) => {
+        const results = [];
 
-		fs.createReadStream(path)
-			.pipe(csv())
-			.on('data', (data) => results.push(data))
-			.on('end', () => {
-				resolve(results);
-			});
-	});
+        fs.createReadStream(path)
+            .pipe(csv({ separator: ';' }))
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                resolve(results);
+            });
+    });
 }
 
 /**
@@ -29,235 +29,270 @@ function readCsv(path) {
  * @return                        element or null
  */
 function getElementByKey(array, key, value) {
-	for (var i = 0; i < array.length; i++) {
-		if (array[i][key] == value) {
-			return array[i];
-		}
-	}
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key] == value) {
+            return array[i];
+        }
+    }
 
-	return null;
+    return null;
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
+
+function metrics(users) {
+    return new Promise(async (resolve) => {
+        var mse_sum = 0;
+        var mae_sum = 0;
+        var count = 0;
+        for (var i = 0; i < users.length; i++) {
+            const user = users[i];
+            const ratings = await db.Rating.findAll({
+                where: {
+                    UserId: user.id,
+                },
+            });
+            const recommendations = await db.Recommendation.findAll({
+                where: {
+                    UserId: user.id,
+                    PlaceId: {
+                        [Op.in]: ratings.map((rating) => {
+                            return rating.PlaceId;
+                        }),
+                    },
+                },
+            });
+
+            for (var j = 0; j < recommendations.length; j++) {
+                const predicted = 1 + (5 - 1) * recommendations[j].probability;
+                const observed = getElementByKey(ratings, 'PlaceId', recommendations[j].PlaceId).rating;
+
+                mse_sum += Math.pow(observed - predicted, 2);
+                mae_sum += Math.abs(predicted - observed);
+                count++;
+            }
+        }
+        // [MSE, MAE]
+        resolve([(1 / count) * mse_sum, (1 / count) * mae_sum]);
+    });
 }
 
 async function main() {
-	var spinner = ora('Cleaning database').start();
-	await db.Place.destroy({
-		where: {
-			id: {
-				[Op.not]: null,
-			},
-		},
-		force: true,
-	});
-	await db.Category.destroy({
-		where: {
-			id: {
-				[Op.not]: null,
-			},
-		},
-		force: true,
-	});
-	await db.User.destroy({
-		where: {
-			id: {
-				[Op.not]: null,
-			},
-		},
-		force: true,
-	});
-	spinner.succeed();
+    var spinner = ora('Cleaning database').start();
+    await db.sequelize.sync({ force: true });
+    spinner.succeed();
 
-	spinner = ora('Reading movies.csv').start();
-	const movies = await readCsv('./data/movies.csv');
-	spinner.succeed();
+    spinner = ora('Reading movies').start();
+    var movies = await readCsv('./data/movies.dat');
+    spinner.succeed();
 
-	spinner = ora('Reading ratings.csv').start();
-	const ratings = await readCsv('./data/ratings.csv');
-	spinner.succeed();
+    spinner = ora('Reading ratings').start();
+    const ratings = await readCsv('./data/ratings.dat');
+    spinner.succeed();
 
-	const genres = [];
-	const users = [];
+    const genres = [];
+    const users = [];
 
-	// Get all possible genres
-	// Add genres to DB
-	spinner = ora('Getting genres').start();
-	for (let i = 0; i < movies.length; i++) {
-		movies[i].genres = movies[i].genres.split('|');
-		for (let j = 0; j < movies[i].genres.length; j++) {
-			if (!genres.includes(movies[i].genres[j])) {
-				genres.push(movies[i].genres[j]);
-			}
-		}
-	}
-	for (let i = 0; i < genres.length; i++) {
-		genres[i] = await db.Category.create({
-			id: i + 1,
-			name: genres[i],
-			fsqId: genres[i],
-		});
-	}
-	spinner.succeed();
+    // Get all possible genres
+    // Add genres to DB
+    spinner = ora('Getting genres').start();
+    for (let i = 0; i < movies.length; i++) {
+        movies[i].genres = movies[i].genres.split('|');
+        for (let j = 0; j < movies[i].genres.length; j++) {
+            if (!genres.includes(movies[i].genres[j])) {
+                genres.push(movies[i].genres[j]);
+            }
+        }
+    }
+    genres.push('(no genres listed)');
+    for (let i = 0; i < genres.length; i++) {
+        genres[i] = await db.Category.create({
+            id: i + 1,
+            name: genres[i],
+            fsqId: genres[i],
+        });
+    }
+    spinner.succeed();
 
-	// Adding movies to db
-	spinner = ora('Adding movies').start();
-	var dbMovies = [];
-	var dbMovieGenres = [];
-	for (let i = 0; i < movies.length; i++) {
-		dbMovies.push({
-				id: movies[i].movieId,
-				fsqId: movies[i].movieId,
-				gmapsUrl: '',
-				osmId: '',
-				name: movies[i].title,
-				description: '',
-				timeSpent: 0,
-				lat: 0,
-				lon: 0,
-				address: '',
-				postalCode: '',
-				city: '',
-				state: '',
-				country: '',
-				placeUrl: '',
-				phone: '',
-				twitter: '',
-				facebook: '',
-				instagram: '',
-				wikidata: '',
-				wikipedia: '',
-				wheelchair: 0,
-				images: 0,
-			});
+    // Adding movies to db
+    spinner = ora('Adding movies').start();
+    for (let i = 0; i < movies.length; i++) {
+        var movie = await db.Place.create({
+            id: parseInt(movies[i].movieId),
+            fsqId: movies[i].movieId,
+            gmapsUrl: '',
+            osmId: '',
+            name: movies[i].title,
+            description: '',
+            timeSpent: 0,
+            lat: 0,
+            lon: 0,
+            address: '',
+            postalCode: '',
+            city: '',
+            state: '',
+            country: '',
+            placeUrl: '',
+            phone: '',
+            twitter: '',
+            facebook: '',
+            instagram: '',
+            wikidata: '',
+            wikipedia: '',
+            wheelchair: 0,
+            images: 0,
+        });
 
-		if (movies[i].length == 0) {
-			let genre = getElementByKey(genres, 'name', '(no genres listed)');
-			dbMovieGenres.push({
-				PlaceId: movies[i].movieId,
-				CategoryId: genre.id,
-			});
-		} else {
-			for (let j = 0; j < movies[i].genres.length; j++) {
-				let genre = getElementByKey(genres, 'name', movies[i].genres[j]);
-				dbMovieGenres.push({
-					PlaceId: movies[i].movieId,
-					CategoryId: genre.id,
-				});
-			}
-		}
-	}
-	await db.Place.bulkCreate(dbMovies);
-	await db.PlaceCategory.bulkCreate(dbMovieGenres);
-	spinner.succeed();
+        var categories = [];
+        if (movies[i].length == 0) {
+            let genre = getElementByKey(genres, 'name', '(no genres listed)');
+            categories.push(
+                await db.PlaceCategory.create({
+                    PlaceId: movies[i].movieId,
+                    CategoryId: genre.id,
+                })
+            );
+        } else {
+            for (let j = 0; j < movies[i].genres.length; j++) {
+                let genre = getElementByKey(genres, 'name', movies[i].genres[j]);
+                categories.push(
+                    await db.PlaceCategory.create({
+                        PlaceId: movies[i].movieId,
+                        CategoryId: genre.id,
+                    })
+                );
+            }
+        }
 
-	// Get all possible userIds
-	// Add users to DB
-	// userId,movieId,rating,timestamp
-	spinner = ora('Getting users').start();
-	var dbUsers = [];
-	var dbUserViews = [];
-	for (let i = 0; i < ratings.length; i++) {
-		if (!users.includes(ratings[i].userId)) {
-			users.push(ratings[i].userId);
-			dbUsers.push({
-				id: ratings[i].userId,
-				name: 'User ' + ratings[i].userId,
-				surname: '',
-				country: '',
-				email: 'user' + ratings[i].userId + '@test',
-				password: '',
-				attempts: 0,
-				lastAttempt: new Date(2021, 1, 1),
-				lastNeighbor: new Date(2021, 1, 1),
-				lastRecommended: new Date(2021, 1, 1),
-				views: 0,
-				notificationsPlans: false,
-				notificationsRatings: false,
-			});
+        movies[i] = movie;
+        movie.Categories = categories;
+    }
+    spinner.succeed();
 
-			for (let j = 0; j < genres.length; j++) {
-				dbUserViews.push({
-					UserId: ratings[i].userId,
-					CategoryId: genres[j].id,
-					views: 0,
-				});
-			}
-		}
-	}
-	await db.User.bulkCreate(dbUsers);
-	await db.UserView.bulkCreate(dbUserViews);
-	spinner.succeed();
+    // Get all possible userIds
+    // Add users to DB
+    // userId,movieId,rating,timestamp
+    spinner = ora('Adding users').start();
+    for (let i = 0; i < ratings.length; i++) {
+        if (!users.includes(ratings[i].userId)) {
+            users.push(ratings[i].userId);
+            await db.User.create({
+                id: parseInt(ratings[i].userId),
+                name: 'User ' + ratings[i].userId,
+                surname: '',
+                country: '',
+                email: 'user' + ratings[i].userId + '@test',
+                password: '',
+                attempts: 0,
+                lastAttempt: new Date(2021, 1, 1),
+                lastNeighbor: new Date(2021, 1, 1),
+                lastRecommended: new Date(2021, 1, 1),
+                views: 0,
+                notificationsPlans: false,
+                notificationsRatings: false,
+            });
 
-	spinner = ora('Adding ratings').start();
-	for (let i = 0; i < ratings.length; i++) {
-		// Add ratings
-		await db.Rating.create({
-			UserId: ratings[i].userId,
-			PlaceId: ratings[i].movieId,
-			rating: Math.round(ratings[i].rating),
-		});
+            var dbUserViews = [];
+            for (let j = 0; j < genres.length; j++) {
+                dbUserViews.push({
+                    UserId: parseInt(ratings[i].userId),
+                    CategoryId: genres[j].id,
+                    views: 0,
+                });
+            }
+            await db.UserView.bulkCreate(dbUserViews);
+        }
+    }
+    spinner.succeed();
 
-		// Get user
-		let user = await db.User.findOne({
-			where: {
-				id: ratings[i].userId,
-			},
-		});
+    spinner = ora('Adding ratings').start();
+    await sleep(2000);
+    for (let i = 0; i < ratings.length; i++) {
+        // Get movie
+        let movie = getElementByKey(movies, 'id', parseInt(ratings[i].movieId));
 
-		// Update UserView
-		let movie = await db.Place.findOne({
-			where: {
-				id: ratings[i].movieId,
-			},
-			includes: [
-				{
-					model: db.Category,
-				},
-			],
-		});
+        // Add ratings
+        await db.Rating.create({
+            UserId: parseInt(ratings[i].userId),
+            PlaceId: parseInt(ratings[i].movieId),
+            rating: parseInt(ratings[i].rating),
+        });
 
-		movie.rating = (movie.rating * movie.count + Math.round(ratings[i].rating)) / (movie.count + 1);
-		movie.count = movie.count + 1;
-		await movie.save();
+        // Get user
+        let user = await db.User.findOne({
+            where: {
+                id: parseInt(ratings[i].userId),
+            },
+        });
 
-		for (let j = 0; j < movie.Categories.length; j++) {
-			let userView = await db.UserView.findOne({
-				where: {
-					UserId: user.id,
-					CategoryId: movie.Categories[j].id,
-				},
-			});
-			userView.views = userView.views + 1;
-			await userView.save();
-		}
-		user.views = user.views + movie.Categories.length;
-		await user.save();
-	}
-	spinner.succeed();
+        movie.rating = (movie.rating * movie.count + Math.round(ratings[i].rating)) / (movie.count + 1);
+        movie.count = movie.count + 1;
+        await movie.save();
 
-	console.log(new Date());
-	spinner = ora('Generating recommendations').start();
-	for (let i = 0; i < 20; i++) {
-		db.User.update(
-			{
-				lastNeighbor: new Date(2021, 1, 1),
-				lastRecommended: new Date(2021, 1, 1),
-			},
-			{
-				where: {
-					id: {
-						[Op.not]: null,
-					},
-				},
-			}
-		);
-		let dbUsers = await db.User.findAll();
-		for (var j = 0; j < dbUsers.length; j++) {
-			Recommender.findNeighbors(user);
-			Recommender.generateCFRecommendations(user);
-			Recommender.generateCBRecommendations(user);
-		}
-	}
-	spinner.succeed();
-	console.log(new Date());
+        // Update UserView
+        for (let j = 0; j < movie.Categories.length; j++) {
+            let userView = await db.UserView.findOne({
+                where: {
+                    UserId: user.id,
+                    CategoryId: movie.Categories[j].CategoryId,
+                },
+            });
+            userView.views = userView.views + 1;
+            await userView.save();
+        }
+        user.views = user.views + movie.Categories.length;
+        await user.save();
+    }
+    spinner.succeed();
+
+    console.log(new Date());
+    console.log('Generating recommendations');
+
+    var n = [5, 10, 15, 20];
+    var r = [2, 4, 6, 8, 10];
+    var d = [5, 10, 20];
+
+    // For every N
+    console.log('N;R;D;MSE;MAE');
+    for (var i = 0; i < n.length; i++) {
+        // For every R
+        for (var j = 0; j < r.length; j++) {
+            // Empty recommendations
+            await db.Recommendation.destroy({
+                where: {
+                    UserId: { [Op.not]: null },
+                },
+            });
+            await db.Neighbor.destroy({
+                where: {
+                    UserId1: { [Op.not]: null },
+                },
+            });
+
+            // Generate recommendations for d[i] days
+            for (var k = 0; k < d.length; k++) {
+                //console.log(`Recommending with N = ${n[i]}, R = ${r[j]}, for ${d[k]} days`);
+                let dbUsers = await db.User.findAll();
+                for (var l = 0; l < dbUsers.length; l++) {
+                    await Recommender.findNeighbors(dbUsers[l], n[i], r[j]);
+                    await Recommender.generateCFRecommendations(dbUsers[l]);
+                    await Recommender.generateCBRecommendations(dbUsers[l]);
+                }
+
+                // Compute metrics
+                var m = await metrics(dbUsers);
+                console.log(m);
+                console.log(`${n[i]};${r[j]};${d[k]};${m[0]};${m[1]}`);
+            }
+        }
+    }
+    console.log(new Date());
 }
 
 main();
