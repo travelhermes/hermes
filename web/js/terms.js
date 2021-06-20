5751a3ecc055607b45ef38ddf7628e08d2169b55
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2021-06-20 16:25:55 +0200

    Translated privacy policy and terms and conditions #15

---
+++ web/js/terms.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 8 */
+const pageLang = document.querySelector('html').lang;
+const currentLang = document.querySelector('input#currentLang').value;
+
+const urlParams = new URLSearchParams(window.location.search);
+const langParam = urlParams.get('lang');
+
+// If langParam is not set and pageLanguage differs, redirect
+if (langParam == null && pageLang !== currentLang) {
+    window.location = `/terms/${currentLang}/`;
+    // If langParam is set and differs, redirect
+} else if (langParam != null && langParam !== pageLang) {
+    window.location = `/terms/${langParam}/?lang=${langParam}`;
+}
